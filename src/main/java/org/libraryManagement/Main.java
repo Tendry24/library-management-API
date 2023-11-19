285cadfcb3c010082f0c93e4c20c6c344f8dffe7
Tendry24 <hei.tendry.2@gmail.com>
2023-11-19 19:18:02 +0300

    feat : add one test on main

--- src/main/java/org/libraryManagement/Main.java
+++ src/main/java/org/libraryManagement/Main.java
@@ -1,8 +1,20 @@
 package org.libraryManagement;
 
+import org.libraryManagement.models.Author;
+import org.libraryManagement.repository.AuthorCrudOperations;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
+        AuthorCrudOperations find = new AuthorCrudOperations();
+
+        List<Author> author = find.findAll();
+
+        for (Author authors : author) {
+            System.out.println(authors);
+        }
     }
 
     }
