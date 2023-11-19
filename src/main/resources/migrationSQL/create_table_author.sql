823b11f0c9b2042f959a4668170c9c22543a9fab
Tendry24 <hei.tendry.2@gmail.com>
2023-11-19 18:58:20 +0300

    feat : add sql file for admin and book

---
+++ src/main/resources/migrationSQL/create_table_author.sql
@@ -0,0 +1,7 @@
+create table if not exists author (
+                                      id serial primary key,
+                                      name varchar(250) not null,
+                                      gender char check ( gender = 'M' or gender = 'F' )
+    );
+
+insert into author values ('Jean' , 'M'),('Lara' , 'F'),('Yuu','F');
